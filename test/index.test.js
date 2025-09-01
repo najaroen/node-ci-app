d8a5799338266c88d9b84711ed19eb4d68013333
najaroen <najaroen_tik@live.com>
2025-09-01 15:17:38 +0700

    test

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+
+
+describe('Math functions', () => {
+  it('should add two numbers correctly', () => {
+    expect("Hello How long").to.equal("Hello");
+  });
+
+  it('should multiply two numbers correctly', () => {
+    // expect(multiply(4, 5)).to.equal(20);
+  });
+});
